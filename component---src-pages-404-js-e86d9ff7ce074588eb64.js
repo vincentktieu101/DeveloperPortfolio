f66be3c5e5a49ca0fb40dd159caceb458dc0accf
vincentktieu101@gmail.com <vincentktieu101@gmail.com>
2021-04-13 14:05:04 -0700

    Updates

---
+++ component---src-pages-404-js-e86d9ff7ce074588eb64.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[883],{9616:function(t,e,n){"use strict";n.r(e);var u=n(7294),a=n(2545),l=n(2335);e.default=function(){return u.createElement(a.Z,null,u.createElement(l.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-e86d9ff7ce074588eb64.js.map

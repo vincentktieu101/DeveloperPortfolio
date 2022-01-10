b329f143fec5de85db82f45dae570f6a44a2c475
vincentktieu101@gmail.com <vincentktieu101@gmail.com>
2022-01-09 19:17:13 -0800

    updated resume

--- src/objects/resume-link.tsx
+++ src/objects/resume-link.tsx
@@ -1,5 +1,8 @@
 const resumeLinks = [
-  // changed link to vincentktieu.com
+  // made bullets less specific to working on an MVP and more focused towards impact and technical accomplishment
+  // also added full-stack to job positions
+  "https://drive.google.com/file/d/1bzaPkRKDJM-4bGl41XF3pifm3PyV_imQ/view?usp=sharing",
+  // changed link from vincentktieu.netlify.app to vincentktieu.com
   "https://drive.google.com/file/d/1_0SpsFd2zQmTJe3wsN-qy_ck-IqQJLxG/view",
   "https://drive.google.com/file/d/1Ch2iSVpyJ4ceXsiwBEqqW-Fz9l3a6uW0/view?usp=sharing",
 ];
